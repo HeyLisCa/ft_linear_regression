862a42d46f0b7c9aa9cd02d10c1245ceb90680f7
HeyLisCa <elisalamadon@gmail.com>
2024-07-15 16:20:38 +0200

    name = main

--- prediction.py
+++ prediction.py
@@ -2,24 +2,26 @@ import numpy as np
 
 def predict_price(mileage, theta, x_mean, x_std):
     normalized_mileage = (mileage - x_mean) / x_std
-    return theta[0] + theta[1] * normalized_mileage #estimatePrice(mileage) = θ0 + (θ1 ∗ mileage)
+    return theta[0] + theta[1] * normalized_mileage
 
-theta = np.loadtxt('theta_values.txt')
-x_mean, x_std = np.loadtxt('normalization_params.txt')
 
-while True:
-    user_input = input("Enter the mileage of the car: ")
-    try:
-        mileage = float(user_input)
-        if mileage < 0:
-            print('Invalid input. Please enter a positive value for the mileage.')
-        else:
-            price = predict_price(mileage, theta, x_mean, x_std)
-            
-            if price < 0:
-                print('Error: The mileage is too high to make a price estimation.')
+if (__name__ == "__main__"): 
+    theta = np.loadtxt('theta_values.txt')
+    x_mean, x_std = np.loadtxt('normalization_params.txt')
+
+    while True:
+        user_input = input("Enter the mileage of the car: ")
+        try:
+            mileage = float(user_input)
+            if mileage < 0:
+                print('Invalid input. Please enter a positive value for the mileage.')
             else:
-                print(f'The estimated price for a car with {mileage} km is {price} euros.')
-            break
-    except ValueError:
-        print('Invalid input. Please enter a numeric value for the mileage.')
+                price = predict_price(mileage, theta, x_mean, x_std)
+                
+                if price < 0:
+                    print('Error: The mileage is too high to make a price estimation.')
+                else:
+                    print(f'The estimated price for a car with {mileage} km is {price} euros.')
+                break
+        except ValueError:
+            print('Invalid input. Please enter a numeric value for the mileage.')
